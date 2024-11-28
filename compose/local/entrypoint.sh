1353675012190ec6ca14d0b227753dfd52b193f0
gero <calogerogiglio098@gmail.com>
2024-11-28 18:46:24 +0100

    feat: add based django project

---
+++ compose/local/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+rm -f ./*.pid
+
+# FOR PRODUCTION
+# printf "Checking PostgreSQL availability...\n"
+# until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -q
+# do
+#   printf "Database unavailable, waiting 1 second...\n"
+#   sleep 1;
+# done
+# printf "Database available!\n"
+
+exec "$@"
